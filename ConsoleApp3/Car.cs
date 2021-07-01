5f25753a604fbd75ac2f3ab7e575169b8873af8f
HumbertoCristobalBocos <humcriboc@gmail.com>
2021-07-01 18:03:00 +0200

    Add project files.

---
+++ ConsoleApp3/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Car : Vehicle
+    {
+        public string color = "red";
+        public string model;
+        public int maxSpeed = 200;
+        private int cosa = 1;
+        public int Cosa
+        {
+            get { return cosa; }
+            set { cosa = value; }
+        }
+
+        public Car(string modelName)
+        {
+            model = modelName;
+        }
+
+        public string fullThrottle( string nombre = "Jose")   // method
+        {
+            string texto ="The car is going as fast as it can!" + nombre;
+            fullThrottle2(cosa);
+            return texto;
+        }
+
+        public static void fullThrottle2(int cosa)    // method
+        {
+            Console.WriteLine("The car is going as fast as it can!" + cosa);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car("C5");
+            myObj.color = "verde";
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+            Console.WriteLine(myObj.fullThrottle("Humber"));
+            Console.WriteLine(myObj.fullThrottle());
+            Console.WriteLine(myObj.model);
+            Console.WriteLine(myObj.Cosa);
+            myObj.Cosa = 3;
+            Console.WriteLine(myObj.Cosa);
+            myObj.honk();
+        }
+
+    }
+}
