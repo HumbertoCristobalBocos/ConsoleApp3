dfbb897ef504452500a0a4941706d90c94373a10
HumbertoCristobalBocos <humcriboc@gmail.com>
2021-07-02 20:11:38 +0200

    lanzar excepcion

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,6 +5,17 @@ namespace ConsoleApp3
 {
     class Program
     {
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+            }
+            else
+            {
+                Console.WriteLine("Access granted - You are old enough!");
+            }
+        }
         static void Main(string[] args)
         {
             Car myObj = new Car("C5");
@@ -19,6 +30,7 @@ namespace ConsoleApp3
             Console.WriteLine(myObj.Cosa);
             myObj.honk();
 
+            //checkAge(15);
 
             string writeText = "Hello World!";  // Create a text string
             File.WriteAllText("filename.txt", writeText);  // Create a file and write the content of writeText to it
