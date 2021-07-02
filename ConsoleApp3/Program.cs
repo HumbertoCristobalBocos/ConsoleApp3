96e8e63a43b41f98d0a1d1cc7dbca798d0f0f19a
HumbertoCristobalBocos <humcriboc@gmail.com>
2021-07-02 20:09:33 +0200

    exceptions

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -25,6 +25,16 @@ namespace ConsoleApp3
 
             string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
             Console.WriteLine(readText);  // Output the content
+
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
     }
